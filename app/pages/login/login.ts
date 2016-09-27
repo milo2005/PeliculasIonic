e56c5d5f5939dee73ced08dfb565ce8d8fef252a
Dario Chamorro <dario.chamorro.v@gmail.com>
2016-09-27 15:56:58 -0500

    Login implemented

---
+++ app/pages/login/login.ts
@@ -0,0 +1,20 @@
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {HomePage} from '../home/home';
+
+@Component({
+    templateUrl:'build/pages/login/login.html'
+})
+export class LoginPage {
+
+    user:string;
+    pass:string;
+
+    constructor(private nav:NavController){}
+
+    goToHome(){
+        console.log("Usuario:"+this.user+" Password:"+this.pass);
+        this.nav.push(HomePage);
+    }
+
+}
